4c40c5ab990f5ee7dc8968af7ef3719a75108e1d
YoungUk0126 <70872187+YoungUk0126@users.noreply.github.com>
2022-08-14 23:35:15 +0900

    coin
    준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
    
    동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-n = int(input())
+N,K = map(int, input().split())
+list = [ int(input()) for x in range(0,N)]
+result = 0
+for i in range(N-1, -1 ,-1):
+    if K == 0:
+        break
+    if list[i] > K:
+        continue
+    result += K // list[i]
+    K = K % list[i]
 
-for i in range(1,10):
-    result = n*i
-    print("{} * {} = {}".format(n,i,result))
-
-
+print(result)
