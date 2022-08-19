400503c9c9f6632da6ba1808e8635488def73e95
YoungUk0126 <70872187+YoungUk0126@users.noreply.github.com>
2022-08-19 23:58:49 +0900

    coin
    동전게임

---
+++ coin.py
@@ -0,0 +1,13 @@
+#Coin 게임
+N,K = map(int, input().split())
+list = [ int(input()) for x in range(0,N)]
+result = 0
+for i in range(N-1, -1 ,-1):
+    if K == 0:
+        break
+    if list[i] > K:
+        continue
+    result += K // list[i]
+    K = K % list[i]
+
+print(result)
