28c4971e3986d4e8d83b2bdc037ea3f19534051d
YoungUk0126 <70872187+YoungUk0126@users.noreply.github.com>
2022-08-24 09:18:04 +0900

    잃어버린 괄호

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
-"""설탕 배달 2839번
-숨겨져 있는 규칙을 찾는게 재밌었다.
-7이라는 가장 애매한 숫자를 생각하지 못하고 코딩하여 헤매느라 고생하였다."""
+"""잃어버린 괄호 1541번
+-를 가장 마지막에 해야 한다는 대략적인 그림을 그리고 있었으나
+그냥 -를 기준으로 모든 숫자들을 분리한 후 +를 하지않고
+모두 -처리를 해준다는 생각을 떠올리기가 어려웠다."""
 
-data = int(input())
+data = input().split('-')
 
-tmp = 0
-#if(data < 3):
-#    print(-1)
-if(data < 5 and data % 3 != 0):
-    print(-1)
-else:
-    if(data % 5 == 0):
-        print(int(data/5))
-    elif(data % 5 == 4):
-        tmp = data - 9#3*3
-        tmp = tmp / 5
-        print(int(tmp + 3))
-    elif(data % 5 == 3):
-        tmp = data - 3
-        tmp = tmp / 5
-        print(int(tmp + 1))
+result = 0
+for i in data[0].split('+'):
+    result += int(i)
 
-    elif(data % 5 == 2):
-        if(data == 7):
-            print(-1)
-        else:
-            tmp = data - 12
-            tmp = tmp / 5
-            print(int(tmp + 4))
-    elif(data % 5 == 1):
-        tmp = data - 6
-        tmp = tmp / 5
-        print(int(tmp + 2))
+for i in data[1:]:
+    for j in i.split('+'):
+        result -= int(j)
+
+print(result)
 
 
 
