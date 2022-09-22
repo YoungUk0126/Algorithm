862838286394e8a8e5a9b0720c7a7e01636037ce
YoungUk0126 <70872187+YoungUk0126@users.noreply.github.com>
2022-09-23 02:35:01 +0900

    BFS 기초(1926 그림)

--- main.py
+++ main.py
@@ -1,63 +1,70 @@
 
-"""2667번 단지번호붙이기
-1. 아이디어
- - 2중 for, 값 1 && 방문X => DFS
- - DFS를 통해 찾은 값을 저장 후 정렬 해서 출력
- 
-2. 자료구조
- - 그래프 저장 : int[][]
- - 방문여부 : bool[][]
- - 결과값 : int[]
- 
- map 만들고 visited를 만드는건 알고 있었지만
- 위아래로 움직이는 값의 배열을 따로 만들어 4방향을 탐색하고
- 이어지는 모든 숫자들을 탐색 후 그 값을 result 배열에 넣고
- 하는 과정들을 상상하기가 어려워 유튜브 강의 영상을 참고 하였다.
- DFS,BFS는 특히나 어려운 것 같다.
-"""
-#이거는 습관화 하자
+"""1926 그림
+ 1. 아이디어
+ - 2중 For문 돌리며 값이 1이고 방문하지 않은 경우 BFS를 실행한다.
+ - BFS 돌면서 그림 개수 +1, 최대값을 갱신한다.
+
+ 2. 자료구조
+ - 그래프 전체 지도 : int[][]
+ - 방문 : bool[][]
+ - Queue(BFS)
+
+역시나 BFS 예제로 외워야할 부분은 외우면서 진행하였다.
+DFS와 마찬가지로 계속 연습하면서 내것으로 만들어야 겠다."""
+
 import sys
 input = sys.stdin.readline
 
+n,m = map(int, input().split())
 
-N = int(input())
+map = [list(map(int, input().split())) for _ in range(n)]
+chk = [[False] * m for _ in range(n)]
 
-map = [list(map(int, input().strip())) for _ in range(N)]
-chk = [[False] * N for _ in range(N)]
-result = []
-each = 0
 dy = [0,1,0,-1]
 dx = [1,0,-1,0]
-def dfs(y,x):
-    global each
-    each += 1
-    for k in range(4):
-        ny = y + dy[k]
-        nx = x + dx[k]
-        if 0<=ny<N and 0<=nx<N:
-            if map[ny][nx] == 1 and chk[ny][nx] == False:
-                chk[ny][nx] = True
-                dfs(ny, nx)
 
 
-for j in range(N):
-    for i in range(N):
-        if map[j][i] == 1 and chk [j][i] == False:
-            # 방문 체크 표시
+def bfs(y,x):
+    result = 1
+    q = [(y,x)]
+    #Queue에 값이 없을 때 까지
+    while q:
+        #Queue에 있는 값을 꺼내
+        ey, ex = q.pop()
+        for k in range(4):
+            # 이동할 y좌표와 x좌표 값을 구해
+            ny = ey + dy[k]
+            nx = ex + dx[k]
+            # 그림의 크기를 넘어가서는 안돼
+            if 0<=ny<n and 0<=nx<m:
+                # 이동 할 좌표가 1이고 방문한 적이 없다면
+                if map[ny][nx] == 1 and chk[ny][nx] == False:
+                    # 그림의 크기 +1 해줌
+                    result += 1
+                    # 방문 표시 해줌
+                    chk[ny][nx] = True
+                    # Queue에 현재 값을 넣어준다.
+                    q.append((ny,nx))
+    return result
+
+
+cnt = 0
+maxv = 0
+
+for j in range(n):
+    for i in range(m):
+        # map에서 1이며 방문하지 않은 경우
+        if map[j][i] == 1 and chk[j][i] == False:
+            # 방문처리
             chk[j][i] = True
-            each = 0
-            dfs(j,i)
-            result.append(each)
-            # DFS로 크기 구하기
-            # 크기를 결과 리스트에 넣기
+            # 그림의 개수
+            cnt += 1
+            # 현재 가장 큰 그림과 BFS로 지금 탐색할 그림을 비교하여
+            # 둘 중에 가장 큰 값이 가장 큰 그림이 된다.
+            maxv = max(maxv, bfs(j,i))
 
-result.sort()
-print(len(result))
-for i in result:
-    print(i)
-
-
-
+print(cnt)
+print(maxv)
 
 
 
